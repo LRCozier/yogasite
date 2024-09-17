48c140d31278cd9772dc3d0c8898ed61d158b3bf
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-09-17 17:31:07 +0100

    updated header.jsx with hamburger menu and css

--- yoga/src/Components/Header/header.jsx
+++ yoga/src/Components/Header/header.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import './header.css';
 
 const Header = () =>{
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return(
         <>
         <div>
             <header>
-                <nav>
-                    <ul>
-                        <li>Home</li>
-                        <li>About Me</li>
-                        <li>Classes</li>
-                        <li>Yoga in Education</li>
-                        <li>Contact</li>
+                <nav className={`nav ${isOpen ? 'nav-open' : ''}`}> {/*NAVBAR FOR TABLET/DESKTOP DISPLAY*/}
+                    <ul className="navbar-links">
+                        <li><Link to="/">Home</Link></li>
+                        <li to="/about"><Link>About</Link></li>
+                        <li><Link to="/classes">Classes</Link></li>
+                        <li><Link to="/yogaeducation">Yoga In Education</Link></li>
+                        <li><Link to="/contact"></Link>Contact</li>
                     </ul>
                 </nav>
+                <div className="hamburger" onClick={toggleMenu}> {/*HAMBURGER MENU FOR MOBILE DISPLAY*/}
+                 <div className={`bar ${isOpen ? 'bar-open' : ''}`}></div>
+                 <div className={`bar ${isOpen ? 'bar-open' : ''}`}></div>
+                 <div className={`bar ${isOpen ? 'bar-open' : ''}`}></div>
+                </div>
             </header>
         </div></>
     )
