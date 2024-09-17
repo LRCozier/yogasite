503a5d128975ec0bb8947e5cb8f41cf7275f1108
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-09-17 18:21:46 +0100

    updated footer content and css

--- yoga/src/Components/Footer/Footer.jsx
+++ yoga/src/Components/Footer/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () => {
             <div className='socials'>
             <SocialIcon network='instagram' />
             <SocialIcon network='facebook' />
+            <p>Designed & Built by Luke Rudderham-Cozier</p>
             </div>
         </footer>
         </>
