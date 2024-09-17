f07aaeed682ea8f127d2c213f3ddcb409d94e4e4
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-09-17 17:31:20 +0100

    added header and footer to app.jsx

--- yoga/src/App.jsx
+++ yoga/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './Pages/Hero/hero'
+import Header from './Components/Header/header';
+import Footer from './Components/Footer/Footer';
 import './App.css'
 import AboutMe from './Pages/About/about'
 import Classes from './Pages/Classes/classes'
@@ -9,11 +11,13 @@ function App() {
   return (
     <Router>
       <div className='App'>
+        <Header />
         <Routes>
           <Route path='/' element={<Hero/>} />
           <Route path='/about' element={<AboutMe/>} />
           <Route path='/classes' element={<Classes/>} />
         </Routes>
+        <Footer/>
       </div>
     </Router>
   )
