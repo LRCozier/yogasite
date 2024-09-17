92d7e4d71f54680da7fc2a4d764c9d4a0e59ef05
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-09-17 18:22:01 +0100

    updated header content and css

--- yoga/src/Components/Header/header.jsx
+++ yoga/src/Components/Header/header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import './header.css';
 
 const Header = () =>{
@@ -16,10 +17,10 @@ const Header = () =>{
                 <nav className={`nav ${isOpen ? 'nav-open' : ''}`}> {/*NAVBAR FOR TABLET/DESKTOP DISPLAY*/}
                     <ul className="navbar-links">
                         <li><Link to="/">Home</Link></li>
-                        <li to="/about"><Link>About</Link></li>
+                        <li><Link to="/about">About</Link></li>
                         <li><Link to="/classes">Classes</Link></li>
                         <li><Link to="/yogaeducation">Yoga In Education</Link></li>
-                        <li><Link to="/contact"></Link>Contact</li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </nav>
                 <div className="hamburger" onClick={toggleMenu}> {/*HAMBURGER MENU FOR MOBILE DISPLAY*/}
