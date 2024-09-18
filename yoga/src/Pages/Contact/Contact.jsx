9a85e0175475f500bb1254917277243daf8fb593
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-09-18 20:15:13 +0100

    added contact form to page

---
+++ yoga/src/Pages/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ContactForm from "../../Components/Contact Form/contactForm";
+
+const Contact = () => {
+
+    return(
+        <>
+        <section>
+            <ContactForm/>
+        </section>
+        </>
+    )
+}
+
+export default Contact;
