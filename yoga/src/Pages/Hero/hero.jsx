87eb640b46955e7fd2ae5c0c7ba87f05fd25efb0
Luke Rudderham-Cozier <lrcozier@gmail.com>
2024-09-18 20:41:50 +0100

    removed background image and inserted hero section image

--- yoga/src/Pages/Hero/hero.jsx
+++ yoga/src/Pages/Hero/hero.jsx
@@ -8,8 +8,9 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="container">
-        <h1>Yoga With Claire</h1>
-        <p>Unite Your Body, Spirit & Mind</p>
+        <h1>Yogi Claire</h1>
+        <img src='yogahero.jpg'/>
+        <p>Uniting Your Body, Spirit & Mind</p>
         <div className='button-container'>
           <Link to='/classes'>
           <button className="cta-button">Explore Classes</button>
